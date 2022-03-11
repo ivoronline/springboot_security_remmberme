066cf16f4cd6eb917c1d42abc38fb9456dd1b578
igoreta <ivoronline@gmail.com>
2022-03-11 10:17:36 +0100

    Commit

--- src/main/java/com/ivoronline/springboot_security_remmberme/config/SecurityConfig.java
+++ src/main/java/com/ivoronline/springboot_security_remmberme/config/SecurityConfig.java
@@ -1,30 +1,42 @@
 package com.ivoronline.springboot_security_remmberme.config;
 
+
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+  //PROPERTIES
+  private final UserDetailsService userDetailsService;
+
+  //=================================================================
+  // CONFIGURE
+  //=================================================================
   @Override
   protected void configure(HttpSecurity httpSecurity) throws Exception {
+
+    //ENABLE REMEMBER ME COOKIE
+    httpSecurity.rememberMe().key("something").userDetailsService(userDetailsService);
 
     //DISABLE CSRF
     httpSecurity.csrf().disable();
 
-    //SPECIFY ACCESS TO ENDPOINTS
-    httpSecurity.authorizeRequests()
-      .antMatchers("/hello").hasRole("USER");
+    //SECURE EVERYTHING
+    httpSecurity.authorizeRequests().anyRequest().authenticated();
 
     //CUSTOM LOGIN FORM
-    httpSecurity.formLogin()
-      .loginPage("/MyLoginForm")
-      .loginProcessingUrl("/login");
+    httpSecurity.formLogin();
 
   }
 
 }
+
+
 

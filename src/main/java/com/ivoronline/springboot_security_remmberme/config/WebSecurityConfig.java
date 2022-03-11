ed79890d44a464e653fc40b3ba753e343ac45d90
igoreta <ivoronline@gmail.com>
2022-03-11 16:24:42 +0100

    Commit

--- src/main/java/com/ivoronline/springboot_security_remmberme/config/WebSecurityConfig.java
+++ src/main/java/com/ivoronline/springboot_security_remmberme/config/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.ivoronline.springboot_security_remmberme.config;
 
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
